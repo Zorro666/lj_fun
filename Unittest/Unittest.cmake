a20f37536bde0ccfcf539240c2b8007ba3353d98
Jake <jake@Jakes-Laptop.example.com>
2009-01-16 20:06:31 +0000

    updated unittest project to work with makefile updates changed gold build to use -O4 -ffast-math (now need to fix unittest which is failing)

--- Unittest/Unittest.cmake
+++ Unittest/Unittest.cmake
@@ -1,26 +1,8 @@
 CMAKE_MINIMUM_REQUIRED( VERSION 2.6 )
 PROJECT( UNITTEST )
 
-Find_Package ( SDL REQUIRED )
-Find_Package ( GLUT REQUIRED )
-Find_Package ( SDL_image REQUIRED ) # if using SDL_image
+INCLUDE ( LJ_Engine/LJ_ROOT.cmake )
 
-# Workaround for the non-working REQUIRED flag
-IF ( NOT SDL_FOUND )
-   MESSAGE( FATAL_ERROR "SDL not found!" )
-ENDIF ( NOT SDL_FOUND )
-
-MESSAGE( "CMAKE_BUILD_TYPE = ${CMAKE_BUILD_TYPE}" )
-
-SET( CMAKE_C_FLAGS_DEBUG "-DLJ_DEBUG -g" )
-SET( CMAKE_C_FLAGS_RELWITHDEBINFO "-DLJ_NDEBUG -g" )
-SET( CMAKE_C_FLAGS_RELEASE "-DLJ_GOLD -DLJ_NDEBUG" )
-
-# Global definitions
-SET( GLOBAL_CFLAGS -Wextra -Wall -Werror -Wdeclaration-after-statement -fstrict-aliasing )
-SET( GLOBAL_DEFINITIONS ${GLOBAL_CFLAGS} ${CONFIG_CFLAGS} )
-
-SET( GLOBAL_INCLUDE_DIRECTORIES )
 SET( GLOBAL_INCLUDE_DIRECTORIES ${GLOBAL_INCLUDE_DIRECTORIES} ${UNITTEST_SOURCE_DIR}/LJ_Engine ${UNITTEST_SOURCE_DIR}/Unittest )
  
 # if you don't want the full compiler output, remove the following line
